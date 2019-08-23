b56e3b8f5961e22a69acf707baf008cddc7b0feb
jsh901220 <joe1220@daum.net>
2019-08-23 16:45:41 +0900

    toggleView

---
+++ src/components/organisms/ToggleView/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+import Button from "../../atoms/Button";
+
+const Wrapper = styled("div")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Content = styled("div")`
+  position: relative;
+  height: 0;
+  opacity: 0;
+  overflow: hidden;
+  margin-top: 10px;
+
+  flex-direction: column;
+  align-items: center;
+
+  ${props =>
+    props.visible &&
+    css`
+      height: ${props.height}px;
+      opacity: 1;
+      transition: height 2s ease-out, opacity 4s ease-out;
+    `}
+`;
+
+const ToggleView = ({ height, children }) => {
+  const [visible, setVisible] = useState(false);
+  const toggleViewEvent = () => {
+    setVisible(!visible);
+  };
+  return (
+    <Wrapper>
+      <Button onClick={toggleViewEvent}>What's that?</Button>
+      <Content height={height} visible={visible}>
+        {children}
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default ToggleView;
